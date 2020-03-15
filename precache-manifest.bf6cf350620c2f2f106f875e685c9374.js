5d3fa1852c23c00378a044ece8e28d56b241231f
hamoudy41 <hamoudy41@yahoo.com>
2020-03-15 11:14:04 +0100

    Updates

---
+++ precache-manifest.bf6cf350620c2f2f106f875e685c9374.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5657de0ecf648bdcfd8a4bc9b04e4e3f",
+    "url": "/course-ratings/index.html"
+  },
+  {
+    "revision": "0017212ad0f2f4ebf1cc",
+    "url": "/course-ratings/static/css/main.cd853d54.chunk.css"
+  },
+  {
+    "revision": "4be6963e6606b2fe2f3f",
+    "url": "/course-ratings/static/js/2.ecf5a911.chunk.js"
+  },
+  {
+    "revision": "6d967876016d7f4b7c601154f6d2ed4f",
+    "url": "/course-ratings/static/js/2.ecf5a911.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0017212ad0f2f4ebf1cc",
+    "url": "/course-ratings/static/js/main.ad4ab68a.chunk.js"
+  },
+  {
+    "revision": "52e1cd0c52f2d4c27cff",
+    "url": "/course-ratings/static/js/runtime-main.6b002e73.js"
+  }
+]);
